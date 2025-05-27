7157ee8c1358cf4b6421d3a6e71f9c6be7a715d6
DanielEJones <djonesthe21st@gmail.com>
2025-05-27 16:13:59 +0800

    set up cli skeleton

--- src/snappy/cli.py
+++ src/snappy/cli.py
@@ -1,3 +1,22 @@
+from argparse import ArgumentParser
+from pathlib import Path
+
+from .core import run_tests, review_snaps
+
+
 def main() -> None:
-    print("Hello, World!")
+    parser = ArgumentParser(description="A snapshot test manager.")
+    mode = parser.add_subparsers(dest="mode", required=True, help="Must select a mode of operation")
+
+    # Test mode, to discover and execute tests
+    test = mode.add_parser("test", help="Select a directory to discover tests.")
+    test.add_argument("directory", type=Path, help="Path to test directory.")
+
+    # Review mode, to discover and review snapshots
+    review = mode.add_parser("review", help="Select a directory to review.")
+    review.add_argument("directory", type=Path, help="Path to review.")
+
+    match (args := parser.parse_args()).mode:
+        case "test":   run_tests(args.directory)
+        case "review": review_snaps(args.directory)
 
---
+++ src/snappy/core.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+
+def run_tests(path: Path) -> None:
+    print(f"Running tests found in {str(path)}...")
+
+
+def review_snaps(path: Path) -> None:
+    print(f"Reviewing snaps found in {str(path)}...")
